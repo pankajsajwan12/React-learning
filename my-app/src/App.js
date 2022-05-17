6cb37b638b66f973def135fef2cc0d19cbb159d7
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2022-05-17 12:17:55 +0530

    Assigment1

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Assigment from './components/Assigment';
+function App() {
+  return (
+    <div className="App">
+       <Assigment></Assigment>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Assigment.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Assigment = () => {
+  return (
+    <div className="maindiv">
+            
+    <div>
+        <h3>Mobile Operating System</h3>
+        <ul>
+            <li>Android</li>
+            <li>Bkkberry </li>
+            <li>iPhone</li>
+            <li>Windows Phone</li>
+        </ul>
+    </div>
+        
+    <div>
+        <h3>Mobile Manufacturers</h3>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>;
+      
+    </div>
+  )
+}
+
+export default Assigment
